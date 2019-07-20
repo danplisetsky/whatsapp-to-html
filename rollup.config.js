e08d915a3b7a8c7ca8fb39d35d2d0750857d4fd6
Daniel Plisetsky <danplisetsky@gmail.com>
2019-07-20 16:16:05 +0300

    generate minified outputs

--- rollup.config.js
+++ rollup.config.js
@@ -2,35 +2,62 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/index.ts",
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-    }),
-    babel({
-      exclude: "node_modules/**",
-      extensions: ["ts"],
-    }),
-  ],
-  external: ["fs", "os"],
-  output: [
-    {
-      globals: {
-        "date-and-time": "date-and-time",
+const commonPlugins = [
+  nodeResolve(),
+  commonjs(),
+  typescript({
+    rollupCommonJSResolveHack: true,
+  }),
+  babel({
+    exclude: "node_modules/**",
+    extensions: ["ts"],
+  }),
+];
+
+export default [
+  {
+    input: "src/index.ts",
+    plugins: commonPlugins,
+
+    external: ["fs", "os"],
+    output: [
+      {
+        globals: {
+          "date-and-time": "date-and-time",
+        },
+        format: "cjs",
+        file: "dist/whatsapp-to-html.js",
       },
-      format: "cjs",
-      file: "dist/whatsapp-to-html.js",
-    },
-    {
-      globals: {
-        "date-and-time": "date-and-time",
+      {
+        globals: {
+          "date-and-time": "date-and-time",
+        },
+        format: "esm",
+        file: "dist/whatsapp-to-html.es.js",
       },
-      format: "esm",
-      file: "dist/whatsapp-to-html.es.js",
-    },
-  ],
-};
+    ],
+  },
+  {
+    input: "src/index.ts",
+    plugins: commonPlugins.concat([terser()]),
+    external: ["fs", "os"],
+    output: [
+      {
+        globals: {
+          "date-and-time": "date-and-time",
+        },
+        format: "cjs",
+        file: "dist/whatsapp-to-html.min.js",
+      },
+      {
+        globals: {
+          "date-and-time": "date-and-time",
+        },
+        format: "esm",
+        file: "dist/whatsapp-to-html.es.min.js",
+      },
+    ],
+  },
+];
